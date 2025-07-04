cbd019eb1d0f37b25a2109a5789b781a6fd16c81
kevinpettersson <kevin.petterssons@hotmail.com>
2025-07-04 21:49:45 +0200

    redirects, transfer-encoding implemented

--- browser.py
+++ browser.py
@@ -47,7 +47,10 @@ class URL:
                 self.host, port = self.host.split(":", 1)
                 self.port = int(port)
         
-    def request(self):
+    def request(self, redirect_limit=5):
+        if redirect_limit <= 0:
+            return "Error: Too many redirects"
+        
         if self.scheme == "file":
             return self.handle_file_request()
             
@@ -57,6 +60,7 @@ class URL:
         else:
             # Handling HTTP/HTTPS requests
             s = self.get_socket()
+            s.settimeout(10.0)
 
             request = self.create_http_request()
             
@@ -74,21 +78,60 @@ class URL:
                 header, value = line.split(":", 1)
                 response_headers[header.casefold()] = value.strip() 
 
-            assert "transfer-encoding" not in response_headers
+            #assert "transfer-encoding" not in response_headers
             assert "content-encoding" not in response_headers
-            
-            # Read only as many bytes specified by header
-            content_length = int(response_headers.get("content-length", 0))
-            content = response.read(content_length)
+
+            # Handle status codes 3xx (Redirects)
+            if 300 <= int(status) < 400:
+                redirect = response_headers.get("location")
+                if redirect is not None:
+                    print(f"Redirecting to: {redirect}")
+                    return self.handle_redirects(redirect, redirect_limit-1)
+
+            # Handle reading of data  
+            if response_headers.get("transfer-encoding") == "chunked":
+                content = self.handle_transfer_encoding(response)
+            elif "content-length" in response_headers:
+                content_length = int(response_headers.get("content-length"))
+                content = response.read(content_length)
+            else:
+                s.settimeout(10.0)
+                content = response.read()
 
             # Close socket if specified by header 
             if response_headers.get("connection") == "close":
                 key = (self.host, self.port)
                 if key in URL.open_sockets:
-                    del self.open_sockets[key]
+                    del URL.open_sockets[key]
                 s.close()
 
             return content
+    
+    def handle_transfer_encoding(self, response):
+        body = ""
+        while True:
+            line = response.readline()
+            chunk_size = int(line.strip(), 16)
+            if chunk_size == 0:
+                break
+            body += response.read(chunk_size)
+            response.read(2)
+        return body
+        
+    def handle_redirects(self, redirect, redirect_limit):
+        key = (self.host, self.port)
+        if key in URL.open_sockets:
+            URL.open_sockets[key].close()
+            del URL.open_sockets[key]
+
+        if redirect.startswith("/"):
+            redirect_url = f"{self.scheme}://{self.host}{redirect}"
+        elif "://" not in redirect:
+            redirect_url = f"{self.scheme}://{self.host}/{redirect}"
+        else:
+            redirect_url = redirect 
+        print(f"Following redirect to: {redirect_url}")
+        return URL(redirect_url).request(redirect_limit)
     
     def handle_file_request(self):
         # Handle file requests, just read the local file
@@ -133,11 +176,12 @@ class URL:
         return s
 
     def create_http_request(self):
-        request = f"GET {self.path} HTTP/1.1\r\n"
+        request = f"GET {self.path} HTTP/1.0\r\n"
 
         headers = {
             "Host": self.host,
             "User-Agent": "MyBrowser",
+            "Connection": "close",
         }
 
         for key,value in headers.items():
